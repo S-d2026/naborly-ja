c4d32ce5bb8d5cd4b2ca11be04c8c901708d5627
S-d2026 <shanas_d@yahoo.com>
2026-04-27 10:29:46 -0400

    Add useSearchParams import to page.tsx

--- Downloads/naberly/naberly/app/browse/page.tsx
+++ Downloads/naberly/naberly/app/browse/page.tsx
@@ -3,6 +3,7 @@
 
 
 import Link from 'next/link'
+import { useSearchParams } from 'next/navigation'
 import { useState, useEffect, useCallback, Suspense } from 'react'
 import { getApprovedListings, type Listing } from '@/lib/supabase'
 
